1409649acfbac7f7d7265ef4c5a60448afee0c19
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2024-06-27 21:07:25 -0400

    Initialize modal for player info inputs and create display controller object

--- script.js
+++ script.js
@@ -8,7 +8,16 @@ const gameBoard = (function () {
     const resetGameBoard = () =>
         (gameTiles = [null, null, null, null, null, null, null, null, null]);
 
-    const updateGameBoard = (z, tilePiece) => (gameTiles[z] = tilePiece);
+    const updateGameBoard = (z, tilePiece) => {
+        let updated;
+        if (gameTiles[z] === null) {
+            gameTiles[z] = tilePiece;
+            updated = true;
+        } else {
+            updated = false;
+        }
+        return updated;
+    };
 
     const winningCombos = [
         [0, 1, 2],
@@ -49,23 +58,79 @@ function createPlayer(name, tilePiece) {
     const winAGame = () => {
         score++;
     };
-    const seeScore = () => console.log(score);
+    const getScore = () => {
+        let seeScore = score;
+        return seeScore;
+    };
 
-    return { name, tilePiece, winAGame, seeScore };
+    return { name, tilePiece, winAGame, getScore };
 }
+
+// Display Updater
+
+const displayController = (function () {
+    // document queries
+    const playerOneName = document.querySelector(".player-1-name");
+    const playerOneScore = document.querySelector(".player-1-score");
+    const playerTwoName = document.querySelector(".player-2-name");
+    const playerTwoScore = document.querySelector(".player-2-score");
+
+    const displayGrid = document.querySelector(".tile-grid");
+
+    // functions
+    const updatePlayerName = (playerOne, playerTwo) => {
+        playerOneName.textContent = playerOne.name;
+        playerTwoName.textContent = playerTwo.name;
+    };
+    const updatePlayerScore = (playerOne, playerTwo) => {
+        playerOneScore.textContent = playerOne.getScore();
+        playerTwoScore.textContent = playerTwo.getScore();
+    };
+    const updateGameBoard = (z, tilePiece) => {
+        displayGrid.querySelector(`.cell-${z}`).textContent = tilePiece;
+    };
+
+    const updateTurn = () => {
+        playerOneName.classList.toggle("your-turn");
+        playerTwoName.classList.toggle("your-turn");
+    };
+
+    return { updatePlayerName, updatePlayerScore, updateGameBoard, updateTurn };
+})();
 
 // Game Controller
 
 const game = (function () {
     const initializeGame = () => {
-        // will prompt for player info here in modal
-        // should update document to list player names
-        const playerOne = createPlayer("Sidney", "X"); // testing functionality - to be deleted (still need prompt for user to input info)
-        const playerTwo = createPlayer("Lamb", "🐑"); // testing functionality - to be deleted
-        return [playerOne, playerTwo];
+        const dialog = document.getElementById("dialog");
+        const playGameBtn = dialog.querySelector("#play-game");
+        const playerOneNameInput = document.querySelector("#player-ones-name");
+        const playerOneSymbolInput = document.querySelector(
+            "#player-ones-symbol"
+        );
+        const playerTwoNameInput = document.querySelector("#player-twos-name");
+        const playerTwoSymbolInput = document.querySelector(
+            "#player-twos-symbol"
+        );
+
+        dialog.showModal();
+
+        playGameBtn.addEventListener("click", (event) => {
+            const playerOne = createPlayer(
+                playerOneNameInput.value,
+                playerOneSymbolInput.value
+            );
+            const playerTwo = createPlayer(
+                playerTwoNameInput.value,
+                playerTwoSymbolInput.value
+            );
+            event.preventDefault();
+            dialog.close();
+            playRound(playerOne, playerTwo);
+        });
     };
 
-    const playRound = () => {
+    const playRound = (playerOne, playerTwo) => {
         // play a round
         for (let i = 0; i < 1; i++) {
             //update i to be < 9 when done
@@ -77,17 +142,24 @@ const game = (function () {
             gameBoard.updateGameBoard(2, playerOne.tilePiece);
             gameBoard.updateGameBoard(6, playerTwo.tilePiece);
 
-            // click event to get div index, then update gameboard based on index and turn #
+            // click event to get div index (z), then update gameboard based on index and turn #
 
             if (i % 2 === 0) {
                 let z = 0; //to be deleted
-                // highlight player 1
-                gameBoard.updateGameBoard(z, playerOne.tilePiece);
+                // highlight player 1?
+                if (gameBoard.updateGameBoard(z, playerOne.tilePiece)) {
+                    gameBoard.updateGameBoard(z, playerOne.tilePiece);
+                    displayController.updateGameBoard(z, playerOne.tilePiece);
+                }
             } else {
                 let z = 3; //to be deleted
-                // highlight player 2
-                gameBoard.updateGameBoard(z, playerTwo.tilePiece);
+                // highlight player 2?
+                if (gameBoard.updateGameBoard(z, playerTwo.tilePiece)) {
+                    gameBoard.updateGameBoard(z, playerTwo.tilePiece);
+                    displayController.updateGameBoard(z, playerTwo.tilePiece);
+                }
             }
+            displayController.updateTurn();
 
             // check to see if there was a winner after each move
             let winningPlayer = gameBoard.winnerCheck(
@@ -112,7 +184,5 @@ const game = (function () {
     };
 
     // actually playing a game, calling the following functions defined above
-    let playerOne, playerTwo;
-    [playerOne, playerTwo] = initializeGame();
-    playRound();
+    initializeGame();
 })();
